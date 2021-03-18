f1723fedd3015a9c014e61af43707a18d39201b7
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2021-03-18 18:47:33 +0000

    feat(AWSAutoScaling): update models to latest

--- AWSAutoScaling/AWSAutoScalingModel.m
+++ AWSAutoScaling/AWSAutoScalingModel.m
@@ -2268,6 +2268,7 @@ return [date aws_stringValue:AWSDateISO8601DateFormat1];
              @"scheduledActionName" : @"ScheduledActionName",
              @"startTime" : @"StartTime",
              @"time" : @"Time",
+             @"timeZone" : @"TimeZone",
              };
 }
 
@@ -2330,6 +2331,8 @@ return [date aws_stringValue:AWSDateISO8601DateFormat1];
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
 	return @{
+             @"checkpointDelay" : @"CheckpointDelay",
+             @"checkpointPercentages" : @"CheckpointPercentages",
              @"instanceWarmup" : @"InstanceWarmup",
              @"minHealthyPercentage" : @"MinHealthyPercentage",
              };
@@ -2429,6 +2432,7 @@ return [date aws_stringValue:AWSDateISO8601DateFormat1];
              @"scheduledActionName" : @"ScheduledActionName",
              @"startTime" : @"StartTime",
              @"time" : @"Time",
+             @"timeZone" : @"TimeZone",
              };
 }
 
@@ -2473,6 +2477,7 @@ return [date aws_stringValue:AWSDateISO8601DateFormat1];
              @"recurrence" : @"Recurrence",
              @"scheduledActionName" : @"ScheduledActionName",
              @"startTime" : @"StartTime",
+             @"timeZone" : @"TimeZone",
              };
 }
 
